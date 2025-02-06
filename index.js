d8355796598830de6dd4d3caeb91d4b9b8fb59f0
venkyroy22 <venkateswarakonetisetty@gmail.com>
2025-02-06 10:44:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose')
+const express = require('express')
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+const menu = require('./schema')
 
-const app = express();
-const port = 3010;
+mongoose.connect(process.env.DB_URL)
+.then(()=>{
+    console.log("Connected")
+})
+.catch(()=>{
+    console.log("Error")
+})
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
-});
+app.put("/menu", async(req,res)=>{
+    try{
+        const {id} = req.query
+        console.log(id)
+        const {name,description,price} = req.body
+        const item = await menu.findById(id);
+    
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+        item.name = name || item.name;
+        item.description = description || item.description;
+        item.price = price || item.price;
+        await item.save()
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+        res.status(200).json({ message: "Menu item updated", item });
+        
+    }
+    catch(error){
+        res.status(500).json({ error: "Server error" });
+    }
+})
+
+app.delete("/menu", async(req,res)=>{
+    try{
+        const {id} = req.query
+        const item = await menu.findById(id);
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+        await menu.findByIdAndDelete(id);
+        res.status(200).json({ message: "Menu item deleted" });
+    }
+    catch(error){
+        res.status(500).json({ error: "Server error" });
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("Running")
+})
